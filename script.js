4e621294f0ca3a69614d376cf0bd6f592d6d21e6
markie816 <markrennenberg@hotmail.com>
2023-05-12 07:47:13 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Stappenplannetje
+1. create 3 arrays with different options
+    1.1 een kleur
+    1.2 je lijkt op / je ruikt naar / ...
+    1.3 een dier
+2. create a function which takes a random index of the array and returns it
+3. link it to GitHub
+*/
+console.log("hi");
