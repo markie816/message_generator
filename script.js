04816cebc5ab98b522125d17d6bdcbaeff40429e
markie816 <markrennenberg@hotmail.com>
2023-05-15 07:35:52 +0200

    added the working script

--- script.js
+++ script.js
@@ -6,4 +6,21 @@
 2. create a function which takes a random index of the array and returns it
 3. link it to GitHub
 */
-console.log("hi");
+const kleur = ['rood', 'blauw', 'geel', 'paars', 'zwart', 'bruin', 'wit'];
+const vergelijking = ['je lijkt op', 'je ruikt naar', 'je gedraagt je als', 'je neukt', 'je beste vriend is', 'je kan niet zonder'];
+const dier = ['paard', 'varken', 'ezel', 'olifant', 'mier', 'mug', 'stinkdier', 'schildpad', 'hond'];
+
+let bericht = () => {
+   let randomKleur = Math.floor(Math.random() * kleur.length)
+   let gekozenKleur = kleur[randomKleur];
+
+   let randomVergelijking = Math.floor(Math.random() * vergelijking.length)
+   let gekozenVergelijking = vergelijking[randomVergelijking];
+
+   let randomDier = Math.floor(Math.random() * dier.length)
+   let gekozenDier = dier[randomDier];
+
+   return `Je kleur is ${gekozenKleur}. ${gekozenVergelijking} een ${gekozenDier}.`
+};
+
+console.log(bericht())
